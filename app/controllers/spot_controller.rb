a519c384ac5e387ce871cbc8e537315c198b4ec5
luke7515 <luke7515@gmail.com>
2015-11-13 03:02:49 +0000

    Hunjun:1

--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -1,5 +1,6 @@
 class SpotController < ApplicationController
     def create
-        Spot.create(title: params[:tit], content: params[:con])
+        Spot.create(title: params[:tit], content: params[:con], shigan: params[:time], user_id: current_user.id, lat: params[:latitude], lng: params[:longitude])
+        redirect_to :root
     end
 end
