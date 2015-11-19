bccf72174d3a11978d3061dc5d8ff0446946b9a6
luke7515 <luke7515@gmail.com>
2015-11-19 06:09:43 +0000

    hunjun edit bug fixxed

--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -2,7 +2,7 @@ class SpotController < ApplicationController
     def create
         a = params[:tit].gsub "\n", " "
         b = a.gsub "\r", " "
-        c = params[:tit].gsub "\n", " "
+        c = params[:con].gsub "\n", " "
         d = c.gsub "\r", " "        
         Spot.create(title: b, content: d, shigan: params[:time], user_id: current_user.id, lat: params[:latitude], lng: params[:longitude], button: params[:btntype])
         redirect_to :root
@@ -17,9 +17,13 @@ class SpotController < ApplicationController
         
     end
     def update
+        a = params[:tit].gsub "\n", " "
+        b = a.gsub "\r", " "
+        c = params[:con].gsub "\n", " "
+        d = c.gsub "\r", " "   
         pp = Spot.find(params[:id])
-        pp.title = params[:tit]
-        pp.content = params[:con]
+        pp.title = b
+        pp.content = d
         pp.lat = params[:latitude]
         pp.lng = params[:longitude]
         pp.save
