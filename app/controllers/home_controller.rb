d0c212f71955883fc9c797f08d8f7bb2ca81d8eb
luke7515 <luke7515@gmail.com>
2015-11-19 05:06:10 +0000

    hunjun plus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @findmemo = Spot.all
     @memos = Spot.where(user_id: current_user.id)
   end
 end
--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -1,9 +1,9 @@
 class SpotController < ApplicationController
     def create
-        a = params[:tit].gsub "\n", "<br>"
-        b = a.gsub "\r", "<br>"
-        c = params[:tit].gsub "\n", "<br>"
-        d = c.gsub "\r", "<br>"        
+        a = params[:tit].gsub "\n", " "
+        b = a.gsub "\r", " "
+        c = params[:tit].gsub "\n", " "
+        d = c.gsub "\r", " "        
         Spot.create(title: b, content: d, shigan: params[:time], user_id: current_user.id, lat: params[:latitude], lng: params[:longitude], button: params[:btntype])
         redirect_to :root
     end
